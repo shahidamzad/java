b2238edbc406315ca31df4993fca0502afd4aebf
Your Name <shahidamzad07@gmail.com>
2026-03-24 03:54:47 +0530

    day-2

---
+++ basic/arithmeticOperationsOnDouble.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class arithmeticOperationsOnDouble {
+    static void main(String[] args) {
+        double x = 5 , y = 2 ;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println((float) x/y);
+        System.out.println((double) x/y);
+    }
+}
