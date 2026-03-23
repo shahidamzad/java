f1a2f91129392959719a54073c9a132cf2d46891
Your Name <shahidamzad07@gmail.com>
2026-03-24 03:47:33 +0530

    day one of java learning.

---
+++ basic/arithmeticOperation.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class arithmeticOperation {
+    static void main(String[] args) {
+        int x = 5 , y = 2 ;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println((float) x/y);
+        System.out.println((double) x/y);
+    }
+}
---
+++ basic/doubleDataTypes.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class doubleDataTypes {
+    static void main(String[] args) {
+        double  x = 3.124 ;
+        float y = 3.14f ;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
