4e6a67b8ebc02839ed56f2c8e99c3eeb5caa67b3
Your Name <shahidamzad07@gmail.com>
2026-03-24 21:27:38 +0530

    day-2 of learning java.

---
+++ basic/areaOfCircle.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class areaOfCircle {
+    public static void main(String[] args) {
+        double r = 3.7 ;
+        double a = 3.14 * r * r ;
+        System.out.print("Area iof Circle is : " +a);
+
+    }
+}
