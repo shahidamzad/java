b7ca9cdba075f81a6248523b2bd45a24469a4928
Your Name <shahidamzad07@gmail.com>
2026-03-29 00:00:40 +0530

    day-3.

---
+++ basic/variableNamingRules.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class variableNamingRules {
+    static void main(String[] args) {
+        int _32 = 45; // Allowed only ( . ) , ( _ ) and $ before the number
+        int a32 = 3 ; // Allowed
+
+       // int 23 = 4 ; // not allowed
+
+        System.out.print(a32);
+    }
+}
